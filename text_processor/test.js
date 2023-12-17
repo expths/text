66556355e9720ee517e273b69cfa64e7593de774
epca <expths@gmail.com>
2023-12-18 00:14:15 +0900

    测试markdown-it 修改gitgonre

---
+++ text_processor/test.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const markdownit = require('markdown-it')
+
+md = new markdownit()
+fs.readFile('./test.md','utf-8',(error,data)=>{
+    if(error){
+        console.log("Error:",error);
+        return;
+    }
+    const a = md.render(data);
+    console.log(a)
+})
